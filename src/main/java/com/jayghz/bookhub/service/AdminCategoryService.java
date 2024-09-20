893dd6d0aac7114ee6a3bc8ca30b1945c6ce41f0
Jhair <90288906+JayGhz@users.noreply.github.com>
2024-09-20 15:37:17 -0500

    Revert "Feature/crud book"

--- src/main/java/com/jayghz/bookhub/service/AdminCategoryService.java
+++ src/main/java/com/jayghz/bookhub/service/AdminCategoryService.java
@@ -1,16 +1,5 @@
 package com.jayghz.bookhub.service;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import com.jayghz.bookhub.model.entity.Category;
-
 public interface AdminCategoryService {
-    List<Category> getAll();
-    Page<Category> paginate(Pageable pageable);  
-    Category create(Category category);
-    Category findById(Integer id);
-    Category update(Integer id, Category updateCategory);
-    void delete(Integer id);
+
 }
