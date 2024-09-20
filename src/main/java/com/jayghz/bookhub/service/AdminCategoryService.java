aa456815864e240b58b9195804739d2d801b2766
JayGhz <jaykansas7@gmail.com>
2024-09-20 02:06:54 -0500

    feat(backend): category CRUD implementation

--- src/main/java/com/jayghz/bookhub/service/AdminCategoryService.java
+++ src/main/java/com/jayghz/bookhub/service/AdminCategoryService.java
@@ -1,5 +1,16 @@
 package com.jayghz.bookhub.service;
 
-public interface AdminCategoryService {
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.jayghz.bookhub.model.entity.Category;
+
+public interface AdminCategoryService {
+    List<Category> getAll();
+    Page<Category> paginate(Pageable pageable);  
+    Category create(Category category);
+    Category findById(Integer id);
+    Category update(Integer id, Category updateCategory);
+    void delete(Integer id);
 }
