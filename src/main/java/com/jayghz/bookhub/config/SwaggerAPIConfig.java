2c330c806cafe36da80bb6b98fce247bd08c564a
JayGhz <jaykansas7@gmail.com>
2024-10-18 13:34:38 -0500

    refactor: Update SwaggerAPIConfig to include JWT security configuration

--- src/main/java/com/jayghz/bookhub/config/SwaggerAPIConfig.java
+++ src/main/java/com/jayghz/bookhub/config/SwaggerAPIConfig.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
+import java.util.List;
 @Configuration
 public class SwaggerAPIConfig {
     @Value("${bookhub.openapi.dev-url}")
@@ -17,12 +21,12 @@ public class SwaggerAPIConfig {
 
     @Bean
     public OpenAPI myOpenAPI(){
-        //Definir el servidor de desarrollo
+        // Definir el servidor de desarrollo
         Server devServer = new Server();
         devServer.setUrl(devUrl);
         devServer.setDescription("Development Server");
 
-        //Informacion de contacto
+        // Informacion de contacto
         Contact contact = new Contact();
         contact.setEmail("jaykansas7@gmail.com");
         contact.setName("JayGhz");
@@ -30,7 +34,7 @@ public class SwaggerAPIConfig {
 
         License mitLicense = new License().name("MIT License").url("https://opensource.org/licenses/MIT");
 
-        //Informacion general de la API
+        // Informacion general de la API
         Info info = new Info()
                 .title("API BookHub venta de libros")
                 .version("1.0")
@@ -39,8 +43,26 @@ public class SwaggerAPIConfig {
                 .termsOfService("https://www.jayghz.com/terms")
                 .license(mitLicense);
 
+        // Configuración de seguridad JWT
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("JWT Authentication");
+
+        Components components = new Components()
+                .addSecuritySchemes("bearerAuth", securityScheme);
+
+        // Requerimiento de seguridad para utilizar en las operaciones
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        
+
         return new OpenAPI()
                 .info(info)
-                .addServersItem(devServer);
+                .servers(List.of(devServer))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+
     }
 }
